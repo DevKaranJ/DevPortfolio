5cb2f77a980e1c30a94c3f697df7071bb1384e53
Karan Joshi <Karan190213@gmail.com>
2023-07-25 18:02:55 +0530

    Perserve Data Script update

--- script.js
+++ script.js
@@ -37,3 +37,23 @@ form.addEventListener('submit', (event) => {
     form.submit();
   }
 });
+
+// Preserve data in the browser
+
+const contactForm = document.querySelector('#form');
+
+function saveFormData() {
+  const formData = {
+    name: contactForm.querySelector("[name='name']").value,
+    email: contactForm.querySelector("[name='email']").value,
+    message: contactForm.querySelector("[name='message']").value,
+  };
+
+  localStorage.setItem('contactFormData', JSON.stringify(formData));
+}
+
+contactForm.addEventListener('input', saveFormData);
+
+// Paste your code below this line
+
+loadFormData();
