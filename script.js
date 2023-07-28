7c8ce0ed2f58e4753925ba07cd8c2d4afb2c59cc
Karan Joshi <Karan190213@gmail.com>
2023-07-28 14:40:56 +0530

    modal popup window

--- script.js
+++ script.js
@@ -64,3 +64,101 @@ function loadFormData() {
 }
 
 loadFormData();
+
+
+
+// Modal Window
+const projects = [
+  {
+    name: 'Multi-Post Stories',
+    cardDetail: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+    skills: ['CSS' , 'HTML' , 'Bootstrap' , 'Ruby'],
+    image: 'assets/ProjectImg/modalProject.png',
+    live_version: '#',
+    link_source: '#',
+  },
+  {
+    name: 'Profesional Art Printing data',
+    cardDetail: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+    skills: ['CSS' , 'HTML' , 'Bootstrap' , 'Ruby'],
+    image: 'assets/ProjectImg/modalProject.png',
+    live_version: '#',
+    link_source: '#',
+  },
+  {
+    name: 'Data Dashboard Healthcare',
+    cardDetail: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+    skills: ['CSS' , 'HTML' , 'Bootstrap' , 'Ruby'],
+    image: 'assets/ProjectImg/modalProject.png',
+    live_version: '#',
+    link_source: '#',
+  },
+  {
+    name: 'Website Portfolio',
+    cardDetail: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+    skills: ['CSS' , 'HTML' , 'Bootstrap' , 'Ruby'],
+    image: 'assets/ProjectImg/modalProject.png',
+    live_version: '#',
+    link_source: '#',
+  },
+  {
+    name: 'Profesional Art Printing data',
+    cardDetail: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+    skills: ['CSS' , 'HTML' , 'Bootstrap' , 'Ruby'],
+    image: 'assets/ProjectImg/modalProject.png',
+    live_version: '#',
+    link_source: '#',
+  },
+  {
+    name: 'Data Dashboard Healthcare',
+    cardDetail: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+    skills: ['CSS' , 'HTML' , 'Bootstrap' , 'Ruby'],
+    image: 'assets/ProjectImg/modalProject.png',
+    live_version: '#',
+    link_source: '#',
+  },
+  {
+    name: 'Website Portfolio',
+    cardDetail: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+    skills: ['CSS' , 'HTML' , 'Bootstrap' , 'Ruby'],
+    image: 'assets/ProjectImg/modalProject.png',
+    live_version: '#',
+    link_source: '#',
+  },
+];
+
+function openModal(index) {
+  const projectInfo = projects[index];
+
+  document.getElementById('modalProjectName').textContent = projectInfo.name;
+  document.getElementById('modalProjectDescription').textContent = projectInfo.cardDetail;
+  
+  const projectImage = document.getElementById('modalProjectImage');
+  projectImage.src = projectInfo.image;
+  
+  const skillsList = document.getElementById('modalProjectSkills');
+  skillsList.innerHTML = '';
+  projectInfo.skills.forEach(skill => {
+    const li = document.createElement('li');
+    li.textContent = skill;
+    skillsList.appendChild(li);
+  });
+
+  document.getElementById('modalLiveVersionLink').href = projectInfo.live_version;
+  document.getElementById('modalSourceLink').href = projectInfo.link_source;
+
+  const modal = document.getElementById('modal');
+  modal.style.display = 'block';
+}
+
+function closeModal() {
+  const modal = document.getElementById('modal');
+  modal.style.display = 'none';
+}
+
+window.onclick = function (event) {
+  const modal = document.getElementById('modal');
+  if (event.target == modal) {
+    modal.style.display = 'none';
+  }
+};
